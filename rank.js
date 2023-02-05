6ddc4fb30d6dc02f0aa771118ac7f8069eb35361
beyza <70069234+beyzasahingoz@users.noreply.github.com>
2023-02-05 16:35:16 +0300

    added imdbrank js file

---
+++ rank.js
@@ -0,0 +1,65 @@
+const listEl = document.querySelector('li');
+let pageNumber = 1;
+const perPage = 4;
+let currentData = [];
+fetch('./movies.json')
+    .then(res => res.json())
+    .then(data => {
+        currentData = data;
+        renderList(data, pageNumber, perPage);
+    });
+function renderList(data, pageNumber, perPage) {
+    listEl.innerHTML = '';
+    const startIndex = (pageNumber - 1) * perPage;
+    const endIndex = startIndex + perPage;
+    const filteredData = data.slice(startIndex, endIndex);
+    filteredData.forEach(post => {
+        listEl.insertAdjacentHTML('beforeend', `
+<div class="list-container">
+<li>
+    <div class="details">
+        <h4>${post.rank} -</h4>
+        <h4>${post.name}</h4>
+    </div>
+    <div class="rating">
+    <h5>${post.director_name}</h5>
+</div>
+    <div class="rating">
+        <h5>${post.imdb_rating}</h5>
+    </div>
+
+    <div class="details">
+        <h5>${post.year}</h5>
+        <h5>${post.genre}</h5>
+
+    </div>
+</li>
+</div>
+`);
+    });
+    renderPagination(data, pageNumber, perPage);
+}
+function renderPagination(data, pageNumber, perPage) {
+    const paginationEl = document.querySelector('.pagination');
+    paginationEl.innerHTML = '';
+
+    const pageCount = Math.ceil(data.length / perPage);
+
+    for (let i = 1; i <= pageCount; i++) {
+        const btn = document.createElement('button');
+        btn.innerText = i;
+        btn.classList.add('pagination-btn');
+
+        if (i === pageNumber) {
+            btn.classList.add('active');
+        }
+
+        btn.addEventListener('click', () => {
+            pageNumber = i;
+            renderList(data, pageNumber, perPage);
+            renderPagination(data, pageNumber, perPage);
+        });
+
+        paginationEl.appendChild(btn);
+    }
+}
